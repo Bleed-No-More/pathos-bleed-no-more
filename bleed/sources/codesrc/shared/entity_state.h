b26519891bdef31c97e6edab2edb2463010b6e32
TheOverfloater <doommusic666@hotmail.com>
2026-09-17 20:18:28 +0200

    Changes as of 2026 September 17th
     - Add "No baked lighting" flag for env_model.
     - Add support for server-side env_model to also make use of baked vertex lighting.
     - Add capping to pitchdown/pitchup max value for up-down mouse look.
     - Add epsilon to CheckMinsMaxs and PointInMinsMaxs to help resolve collision related issues due to edge cases on some doors in the test levels.
     - Increase crouch time from 0.1 seconds to 0.6 seconds.
     -

--- bleed/sources/codesrc/shared/entity_state.h
+++ bleed/sources/codesrc/shared/entity_state.h
@@ -184,7 +184,6 @@ struct entity_state_t
 		speed(0),
 		stamina(0),
 		modelindex(0),
-		vlight_vbo_index(NO_POSITION),
 		ltime(0),
 		nextthink(0),
 		movetype(MOVETYPE_NONE),
@@ -299,7 +298,6 @@ struct entity_state_t
 
 	Int32 modelindex;
 
-	Int32 vlight_vbo_index;
 	byte vlight_styles[MAX_ENTITY_STYLES];
 
 	Vector absmin;
